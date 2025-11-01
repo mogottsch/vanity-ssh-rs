9d5dd4ce08f79d089845094ec338b3a9e5cc21de
Moritz Gottschling <gottschling.moritz@gmail.com>
2025-11-01 10:07:22 +0100

    Utilize new compress batch function

--- src/core/keypair.rs
+++ src/core/keypair.rs
@@ -14,29 +14,33 @@ pub struct KeyPair {
 
 pub fn generate_keypair_batch(batch_size: usize) -> Vec<KeyPair> {
     let mut csprng = OsRng;
-    (0..batch_size)
-        .map(|_| {
-            let mut secret_key = SecretKey::default();
-            csprng.fill_bytes(&mut secret_key);
-            derive_keypair(secret_key)
+    let mut secret_keys = Vec::with_capacity(batch_size);
+    let mut public_points = Vec::with_capacity(batch_size);
+
+    for _ in 0..batch_size {
+        let mut secret_key = SecretKey::default();
+        csprng.fill_bytes(&mut secret_key);
+        let expanded_secret_key = ExpandedSecretKey::from(&secret_key);
+        let public_point = EdwardsPoint::mul_base(&expanded_secret_key.scalar);
+        secret_keys.push(secret_key);
+        public_points.push(public_point);
+    }
+
+    let compressed_points = EdwardsPoint::compress_batch(&public_points);
+
+    secret_keys
+        .into_iter()
+        .zip(compressed_points.into_iter())
+        .map(|(secret_key, compressed)| KeyPair {
+            secret_key,
+            public_key: compressed,
         })
         .collect()
 }
 
-fn derive_keypair(secret_key: SecretKey) -> KeyPair {
-    let expanded_secret_key = ExpandedSecretKey::from(&secret_key);
-    let public_point = EdwardsPoint::mul_base(&expanded_secret_key.scalar);
-    let public_key = public_point.compress();
-
-    KeyPair {
-        secret_key,
-        public_key,
-    }
-}
-
 pub mod bench_helpers {
     use super::*;
-    
+
     #[allow(dead_code)]
     pub fn generate_secret_key() -> SecretKey {
         let mut csprng = OsRng;
@@ -44,17 +48,17 @@ pub mod bench_helpers {
         csprng.fill_bytes(&mut secret_key);
         secret_key
     }
-    
+
     #[allow(dead_code)]
     pub fn expand_secret_key(secret_key: &SecretKey) -> ExpandedSecretKey {
         ExpandedSecretKey::from(secret_key)
     }
-    
+
     #[allow(dead_code)]
     pub fn compute_mul_base(expanded: &ExpandedSecretKey) -> EdwardsPoint {
         EdwardsPoint::mul_base(&expanded.scalar)
     }
-    
+
     #[allow(dead_code)]
     pub fn compress_point(point: &EdwardsPoint) -> CompressedEdwardsY {
         point.compress()
